1d98325d2c9a42ae4d52ab6593c40df7c511e2d8
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-04-30 17:06:26 -0300

    feat: create User and Role entities with relationship

---
+++ ecommerce/src/main/java/com/vinicius/ecommerce/model/User.java
@@ -0,0 +1,47 @@
+package com.vinicius.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_users")
+@EqualsAndHashCode(of = "id")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "account_non_expired")
+    private Boolean accountNonExpired;
+
+    @Column(name = "account_non_locked")
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
+    private Boolean credentialsNonExpired;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id")
+    )
+    private Set<Roles> roles = new HashSet<>();
+}
