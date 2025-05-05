b7c769c563ccef5ea27e7b6b48b75a87ed4a53ac
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-05 14:38:43 -0300

    feat: releasing the endpoint to register users

--- ecommerce/src/main/java/com/vinicius/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.vinicius.ecommerce.config;
 
-import com.vinicius.ecommerce.properties.EncoderProperties;
 import com.vinicius.ecommerce.token.JwtTokenFilter;
 import com.vinicius.ecommerce.token.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,24 +23,28 @@ import java.util.Map;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Autowired
-    private JwtTokenProvider jwtTokenProvider;
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(EncoderProperties encoderProperties) {
+    public PasswordEncoder passwordEncoder() {
         Map<String, PasswordEncoder> encoders = new HashMap<>();
 
         Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
-                encoderProperties.getSecret(),
+                "",
                 8,
-                encoderProperties.getIterations(),
+                185000,
                 Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256
         );
 
         encoders.put("pbkdf2", pbkdf2Encoder);
 
         DelegatingPasswordEncoder passwordEncoder =
-                new DelegatingPasswordEncoder(encoderProperties.getId(), encoders);
+                new DelegatingPasswordEncoder("pbkdf2", encoders);
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
         return pbkdf2Encoder;
     }
@@ -55,25 +57,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                // Desabilita a autenticação básica HTTP (não necessária com JWT)
                 .httpBasic(AbstractHttpConfigurer::disable)
-
-                // Desabilita CSRF (não é necessário com JWT, pois não utilizamos sessões)
                 .csrf(AbstractHttpConfigurer::disable)
-
-                // Adiciona o filtro JWT antes do filtro padrão de autenticação
-                .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
-
-                // Configuração de gerenciamento de sessões: sem sessão (stateless) para não usar sessões no servidor
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                // Configuração de permissões para endpoints específicos
                 .authorizeHttpRequests(authorize -> authorize
-                        // Permite o acesso a certas rotas sem autenticação
-                        .requestMatchers("/api/v1/auth/**").permitAll())
-                .csrf(c -> {})
+                        .requestMatchers("/api/v1/auth/**").permitAll() // Permite o acesso à rota /api/v1/auth/
+                        .anyRequest().authenticated()) // Requer autenticação para outras rotas
                 .build();
     }
+
 
     @Bean
     AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
