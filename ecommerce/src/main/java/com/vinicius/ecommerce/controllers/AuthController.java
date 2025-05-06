aae2a66c0b6d7da1b81c94f2227cff27e15b690e
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-06 14:21:59 -0300

    feat: logging in and generating token

--- ecommerce/src/main/java/com/vinicius/ecommerce/controllers/AuthController.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/controllers/AuthController.java
@@ -2,6 +2,8 @@ package com.vinicius.ecommerce.controllers;
 
 import com.vinicius.ecommerce.records.data.UserDTO;
 import com.vinicius.ecommerce.records.data.UserDetailsDTO;
+import com.vinicius.ecommerce.records.vo.AccountCredentialsVO;
+import com.vinicius.ecommerce.services.AuthService;
 import com.vinicius.ecommerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,9 +20,26 @@ public class AuthController {
     @Autowired
     UserService service;
 
+    @Autowired
+    AuthService authService;
+
     @PostMapping("/register")
     public ResponseEntity<UserDetailsDTO> createdUser(@RequestBody UserDTO data) {
         var userCreated = service.create(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @PostMapping("/signin")
+    public ResponseEntity signIn(@RequestBody AccountCredentialsVO data) {
+        if(checkIfParamIsNotNull(data)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client Request!");
+        }
+        return ResponseEntity.ok(authService.signin(data));
+    }
+
+    private boolean checkIfParamIsNotNull(AccountCredentialsVO data) {
+        return data == null || data.getEmail() == null || data.getEmail().isBlank()
+                || data.getPassword() == null || data.getPassword().isBlank();
     }
 }
