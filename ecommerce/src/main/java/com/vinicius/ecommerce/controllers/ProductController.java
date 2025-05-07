984e1635f9f0e206815bea631a3d1240478d2e54
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-07 16:23:01 -0300

    feat: methods to list all with pagination, update and delete

--- ecommerce/src/main/java/com/vinicius/ecommerce/controllers/ProductController.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/controllers/ProductController.java
@@ -1,9 +1,14 @@
 package com.vinicius.ecommerce.controllers;
 
 import com.vinicius.ecommerce.model.Product;
+import com.vinicius.ecommerce.records.data.ProductDTO;
 import com.vinicius.ecommerce.records.data.ProductDetailsDTO;
 import com.vinicius.ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -36,5 +41,38 @@ public class ProductController {
     public ResponseEntity<ProductDetailsDTO> findById(@PathVariable("id") Long id) {
         var product = service.findById(id);
         return ResponseEntity.ok(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDetailsDTO>> getAllProducts(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size,
+            @RequestParam(value = "direction", defaultValue = "desc") String direction
+    ) {
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "price"));
+        var products = service.getAllProducts(pageable);
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ProductDetailsDTO> update(
+            @PathVariable("id") Long id,
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "description") String description,
+            @RequestParam(value = "price") BigDecimal price,
+            @RequestParam(value = "image") MultipartFile image,
+            @RequestParam(value = "stock") Integer stock) {
+        var data = new ProductDTO(name, description, price, image, stock);
+        return ResponseEntity.ok(service.update(data, id));
+    }
+
+    @DeleteMapping("{id}")
+    @SuppressWarnings("rawtypes")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- ecommerce/src/main/java/com/vinicius/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/services/ProductService.java
@@ -1,9 +1,13 @@
 package com.vinicius.ecommerce.services;
 
 import com.vinicius.ecommerce.model.Product;
+import com.vinicius.ecommerce.records.data.ProductDTO;
 import com.vinicius.ecommerce.records.data.ProductDetailsDTO;
 import com.vinicius.ecommerce.repositories.ProductRepository;
+import com.vinicius.ecommerce.utils.UpdateValues;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +19,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private UpdateValues updateValues;
 
     public ProductDetailsDTO createProduct(Product product, MultipartFile image) {
         String generatedImage = null;
@@ -39,5 +46,36 @@ public class ProductService {
                 .orElseThrow(() -> new RuntimeException("Product not found!"));
 
         return new ProductDetailsDTO(product);
+    }
+
+    public Page<ProductDetailsDTO> getAllProducts(Pageable pageable) {
+        var pageProduct = repository.findAll(pageable);
+        return pageProduct.map(ProductDetailsDTO::new);
+    }
+
+    public ProductDetailsDTO update(ProductDTO data, Long id) {
+        var product = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found!"));
+
+        updateValues.updateIfNotNullOrEmpty(data.name(), product::setName);
+        updateValues.updateIfNotNullOrEmpty(data.description(), product::setDescription);
+        updateValues.updateIfNotNullOrEmpty(data.stock(), product::setStock);
+        updateValues.updateIfNotNullOrEmpty(data.price(), product::setPrice);
+
+        if(data.image() != null && !data.image().isEmpty()) {
+            String imageUpdate = s3Service.updateImage(product, data.image());
+            product.setImageUrl(imageUpdate);
+        }
+
+        var productSave = repository.save(product);
+        return new ProductDetailsDTO(productSave);
+    }
+
+    public void delete(Long id) {
+        var product = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found!"));
+        String filename = s3Service.formatImageName(product.getImageUrl());
+        s3Service.deleteImage(filename);
+        repository.delete(product);
     }
 }
