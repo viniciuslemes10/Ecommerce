029f5b8fbed57e3459ba44c837f817528d300f02
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-12 12:14:57 -0300

    feat: utility to update non-null and valid values

--- ecommerce/src/main/resources/db/migration/V3__crate_table_product.sql
+++ ecommerce/src/main/resources/db/migration/V3__crate_table_product.sql
@@ -1,8 +1,8 @@
 CREATE TABLE tb_products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
-    stock INT NOT NULL
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price DECIMAL(10, 2),
+    image_url VARCHAR(255),
+    stock INT
 );
