ff643430e863b3255ecf29707785710430cec72d
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-13 14:55:08 -0300

    feat: method that searches for the category name with LIKE and ignoring upper and lower case letters

--- ecommerce/src/main/java/com/vinicius/ecommerce/repositories/CategoryRepository.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/repositories/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.vinicius.ecommerce.repositories;
 
 import com.vinicius.ecommerce.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByNameContainingIgnoreCase(String name);
 }
