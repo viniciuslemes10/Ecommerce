2914c476dc12da0ddd2ee869b936a7cba12f122f
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-05-13 13:45:40 -0300

    feat: adding JpaSpecificationExecutor

--- ecommerce/src/main/java/com/vinicius/ecommerce/repositories/ProductRepository.java
+++ ecommerce/src/main/java/com/vinicius/ecommerce/repositories/ProductRepository.java
@@ -2,8 +2,9 @@ package com.vinicius.ecommerce.repositories;
 
 import com.vinicius.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
